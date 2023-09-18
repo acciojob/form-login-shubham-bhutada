d91174ac484a800d35c4955f8b76122c691bc80f
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-18 19:07:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let first = document.QuerySelectorAll("form>input")[0];
+	let second = document.QuerySelectorAll("form>input")[1];
+	alert(first.innerText + second.innerText)
 }
